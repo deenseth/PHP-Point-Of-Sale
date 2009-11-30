74167f3d370d9968fb844e71114841bb5420ac85
Chris Muench <cmuench@me.com>
2009-11-30 18:47:31 -0500

    Put in message on home page about alpha version (to be released soon)

--- application/views/home.php
+++ application/views/home.php
@@ -1,3 +1,3 @@
 <?php $this->load->view("partial/header"); ?>
-
+<h2>Welcome to the alpha version of PHP Point Of Sale. You will find things not done/broken. Please provide any feedback to <a href="mailto:admin@phppointofsale.com">admin@phppointofsale.com</a>
 <?php $this->load->view("partial/footer"); ?>

d152d265746fc4105b8a59677512fd00de6f5ced
Chris Muench <me@chrismuench.com>
2010-02-26 18:26:46 -0500

    Supplier summary report

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -107,6 +107,30 @@ class Reports extends Secure_area
 		$this->load->view("reports/tabular",$data);
 	}
 	
+	//Summary suppliers report
+	function summary_suppliers($start_date, $end_date)
+	{
+		$this->load->model('reports/Summary_suppliers');
+		$model = $this->Summary_suppliers;
+		$tabular_data = array();
+		$report_data = $model->getData(array('start_date'=>$start_date, 'end_date'=>$end_date));
+		
+		foreach($report_data as $row)
+		{
+			$tabular_data[] = array($row['supplier'], to_currency($row['subtotal']), to_currency($row['total']), to_currency($row['tax']),to_currency($row['profit']));
+		}
+
+		$data = array(
+			"title" => $this->lang->line('reports_suppliers_summary_report'),
+			"subtitle" => date('m/d/Y', strtotime($start_date)) .'-'.date('m/d/Y', strtotime($end_date)),
+			"headers" => $model->getDataColumns(),
+			"data" => $tabular_data,
+			"summary_data" => $model->getSummaryData(array('start_date'=>$start_date, 'end_date'=>$end_date))
+		);
+
+		$this->load->view("reports/tabular",$data);
+	}
+	
 	//Summary items report
 	function summary_items($start_date, $end_date)
 	{
--- application/views/reports/listing.php
+++ application/views/reports/listing.php
@@ -1,4 +1,5 @@
 <?php $this->load->view("partial/header"); ?>
+<h1>This page needs to be internationalized</h1>
 <div id="page_title" style="margin-bottom:8px;"><?php echo $this->lang->line('reports_reports'); ?></div>
 <div id="welcome_message"><?php echo $this->lang->line('reports_welcome_message'); ?>
 <ul id="report_list">
@@ -7,6 +8,7 @@
 			<li><a href="<?php echo site_url('reports/summary_sales');?>">Sales</a></li>
 			<li><a href="<?php echo site_url('reports/summary_categories');?>">Categories</a></li>
 			<li><a href="<?php echo site_url('reports/summary_customers');?>">Customers</a></li>
+			<li><a href="<?php echo site_url('reports/summary_suppliers');?>">Suppliers</a></li>
 			<li><a href="<?php echo site_url('reports/summary_items');?>">Items</a></li>
 			<li><a href="<?php echo site_url('reports/summary_employees');?>">Employees</a></li>
 		</ul>

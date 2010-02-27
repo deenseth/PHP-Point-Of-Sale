68ac749418f60b3e99a194671be74f6d9ffd1cc6
Chris Muench <me@chrismuench.com>
2010-02-27 08:03:46 -0500

    Moving creation of sales items temp table to Sale model

--- application/models/reports/report.php
+++ application/models/reports/report.php
@@ -12,7 +12,7 @@ abstract class Report extends Model
 		$this->output->set_header("Pragma: no-cache");
 		
 		//Create our temp table to work with the data in our report
-		$this->createSalesItemsTempTable();
+		$this->Sale->create_sales_items_temp_table();
 	}
 	
 	//Returns the column names used for the report
@@ -23,35 +23,5 @@ abstract class Report extends Model
 	
 	//Returns key=>value pairing of summary data for the report
 	public abstract function getSummaryData(array $inputs);
-	
-	//We create a temp table that allows us to do easy report queries
-	public function createSalesItemsTempTable()
-	{
-		$this->db->query("CREATE TEMPORARY TABLE ".$this->db->dbprefix('sales_items_temp')." 
-		(SELECT date(sale_time) as sale_date, sale_id, comment,payment_type, customer_id, employee_id, item_id, supplier_id, quantity_purchased, item_cost_price, item_unit_price, SUM(percent) as item_tax_percent, 
-		discount_percent, (item_unit_price*quantity_purchased-item_unit_price*quantity_purchased*discount_percent/100) as subtotal,
-		ROUND((item_unit_price*quantity_purchased-item_unit_price*quantity_purchased*discount_percent/100)*(1+(SUM(percent)/100)),2) as total,
-		ROUND((item_unit_price*quantity_purchased-item_unit_price*quantity_purchased*discount_percent/100)*(SUM(percent)/100),2) as tax,
-		(item_unit_price*quantity_purchased-item_unit_price*quantity_purchased*discount_percent/100) - (item_cost_price*quantity_purchased) as profit
-
-		FROM ".$this->db->dbprefix('sales_items')."
-		INNER JOIN ".$this->db->dbprefix('sales')." USING (sale_id) 
-		INNER JOIN ".$this->db->dbprefix('items')." USING (item_id) 
-		LEFT OUTER JOIN ".$this->db->dbprefix('suppliers')." ON  ".$this->db->dbprefix('items').'.supplier_id='.$this->db->dbprefix('suppliers').'.person_id'."
-		LEFT OUTER JOIN ".$this->db->dbprefix('sales_items_taxes')." USING (sale_id, item_id)
-		GROUP BY sale_id, item_id)");
-		
-		//Update null item_tax_percents to be 0 instead of null
-		$this->db->where('item_tax_percent IS NULL');
-		$this->db->update('sales_items_temp', array('item_tax_percent' => 0));
-		
-		//Update null tax to be 0 instead of null
-		$this->db->where('tax IS NULL');
-		$this->db->update('sales_items_temp', array('tax' => 0));
-
-		//Update null subtotals to be equal to the total as these don't have tax
-		$this->db->query('UPDATE '.$this->db->dbprefix('sales_items_temp'). ' SET total=subtotal WHERE total IS NULL');
-		
-	}
 }
 ?>

e35bc37c099b83345b4e7e995d23656681464de2
Chris Muench <me@chrismuench.com>
2010-04-26 11:07:58 -0400

    Indonesia language

---
+++ application/language/indonesia/module_lang.php
@@ -0,0 +1,25 @@
+<?php
+$lang['module_home']='Beranda';
+
+$lang['module_customers']='Pelanggan';
+$lang['module_customers_desc']='Tambah, Update, Hapus, dan Cari pelanggan ';
+
+$lang['module_suppliers']='Pemasok';
+$lang['module_suppliers_desc']='Tambah, Update, Hapus, dan Cari pemasok';
+
+$lang['module_employees']='Karyawan';
+$lang['module_employees_desc']='Tambah, Update, Hapus, dan Cari karyawan';
+
+$lang['module_sales']='Penjualan';
+$lang['module_sales_desc']='Proses penjualan dan retur';
+
+$lang['module_reports']='Laporan';
+$lang['module_reports_desc']='Lihat dan Hasilkan laporan';
+
+$lang['module_items']='Produk/Item';
+$lang['module_items_desc']='Tambah, Update, Hapus, dan Cari produk/items';
+
+$lang['module_config']='Konfigurasi Toko';
+$lang['module_config_desc']='Ubah konfigurasi toko';
+
+?>

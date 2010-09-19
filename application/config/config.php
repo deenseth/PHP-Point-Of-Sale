d3538ceb683f5831b0cb714e005983ee8f39ab60
Chris Muench <cmuench@me.com>
2010-09-19 19:15:43 -0400

    no limit for session

--- application/config/config.php
+++ application/config/config.php
@@ -233,7 +233,7 @@ $config['encryption_key'] = "";
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_expiration']		= 0;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= TRUE;
 $config['sess_table_name']		= 'sessions';

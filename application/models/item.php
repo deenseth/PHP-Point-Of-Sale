d65d639cf6ceb73e2bf50af06a841e19efd50a62
Chris Muench <cmuench@me.com>
2010-09-23 19:09:28 -0400

    Search items in sales interface by item_number

--- application/models/item.php
+++ application/models/item.php
@@ -184,6 +184,16 @@ class Item extends Model
 		{
 			$suggestions[]=$row->item_id.'|'.$row->name;		
 		}
+		
+		$this->db->from('items');
+		$this->db->like('item_number', $search); 
+		$this->db->order_by("item_number", "asc");		
+		$by_item_number = $this->db->get();
+		foreach($by_item_number->result() as $row)
+		{
+			$suggestions[]=$row->item_id.'|'.$row->item_number;		
+		}
+		
 		//only return $limit suggestions
 		if(count($suggestions > $limit))
 		{

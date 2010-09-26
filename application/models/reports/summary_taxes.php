132ee41b5301efd22e9f3f3a7a3fe1a08b4360d9
Chris Muench <cmuench@me.com>
2010-09-26 10:21:09 -0400

    really fixex the summary taxes report

--- application/models/reports/summary_taxes.php
+++ application/models/reports/summary_taxes.php
@@ -23,9 +23,13 @@ class Summary_taxes extends Report
 		item_unit_price * quantity_purchased - item_unit_price * quantity_purchased * discount_percent /100
 		) * ( percent /100 ) , 2 ) AS tax
 		FROM ".$this->db->dbprefix('sales_items_taxes')."
-		JOIN ".$this->db->dbprefix('sales_items')." USING(sale_id,item_id,line)) as temp_taxes
+		JOIN ".$this->db->dbprefix('sales_items')." ON "
+		.$this->db->dbprefix('sales_items').'.sale_id='.$this->db->dbprefix('sales_items_taxes').'.sale_id'." and "
+		.$this->db->dbprefix('sales_items').'.item_id='.$this->db->dbprefix('sales_items_taxes').'.item_id'." and "
+		.$this->db->dbprefix('sales_items').'.line='.$this->db->dbprefix('sales_items_taxes').'.line'
+		." JOIN ".$this->db->dbprefix('sales')." ON ".$this->db->dbprefix('sales_items_taxes').".sale_id=".$this->db->dbprefix('sales').".sale_id
+		WHERE date(sale_time) BETWEEN '".$inputs['start_date']."' and '".$inputs['end_date']."') as temp_taxes
 		GROUP BY percent");
-		
 		return $query->result_array();
 	}
 	

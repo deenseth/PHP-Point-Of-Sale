f7a0eb689c0aee88f1c1e224c842bf0e48e69416
Chris Muench <cmuench@me.com>
2010-09-30 11:35:30 -0400

    10.5-->10.6

--- application/views/partial/footer.php
+++ application/views/partial/footer.php
@@ -1,6 +1,6 @@
 </div>
 </div>
-<div id="footer"> <?php echo $this->lang->line('common_you_are_using_phppos')?> 10.5.
+<div id="footer"> <?php echo $this->lang->line('common_you_are_using_phppos')?> 10.6.
 <?php echo $this->lang->line('common_please_visit_my'); ?> <a href="http://www.phppointofsale.com" target="_blank"><?php echo $this->lang->line('common_website'); ?></a> <?php echo $this->lang->line('common_learn_about_project'); ?>.</div>
 </body>
 </html>

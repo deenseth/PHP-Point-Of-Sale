7e244a46b03d7dd919e4f101abc9449d1615fd2a
Chris Muench <cmuench@me.com>
2010-10-02 17:59:06 -0400

    Quantity editing now allowed in bulk update

--- application/views/items/form_bulk.php
+++ application/views/items/form_bulk.php
@@ -98,16 +98,6 @@ echo form_open('items/bulk_update/',array('id'=>'item_form'));
 	</div>
 </div>
 <div class="field_row clearfix">	
-<?php echo form_label($this->lang->line('items_quantity').':', 'quantity',array('class'=>'wide')); ?>
-	<div class='form_field'>
-	<?php echo 'Quantity Bulk Update Disabled; Use Excel Import'//Ramel Inventory Tracking//echo form_input(array(
-		//'name'=>'quantity',
-		//'id'=>'quantity')
-	//);?>
-	</div>
-</div>
-
-<div class="field_row clearfix">	
 <?php echo form_label($this->lang->line('items_reorder_level').':', 'reorder_level',array('class'=>'wide')); ?>
 	<div class='form_field'>
 	<?php echo form_input(array(

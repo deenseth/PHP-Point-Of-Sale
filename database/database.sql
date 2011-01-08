acbcc9821c0ea4eaf0820b4fcd3157126398022f
Chris Muench <me@chrismuench.com>
2011-01-08 10:27:15 -0500

    Added permission insert for giftcards

--- database/database.sql
+++ database/database.sql
@@ -250,7 +250,8 @@ INSERT INTO `phppos_permissions` (`module_id`, `person_id`) VALUES ('config', 1)
 ('receivings', 1),
 ('reports', 1),
 ('sales', 1),
-('suppliers', 1);
+('suppliers', 1),
+('giftcards', 1);
 
 -- --------------------------------------------------------
 
--- database/database_10.7-10.8.sql
+++ database/database_10.7-10.8.sql
@@ -9,3 +9,5 @@ CREATE TABLE IF NOT EXISTS `phppos_giftcards` (
 
 INSERT INTO `phppos_modules` (`name_lang_key`, `desc_lang_key`, `sort`, `module_id`) VALUES
 ('module_giftcards', 'module_giftcards_desc', 9, 'giftcards');
+
+INSERT INTO `phppos_permissions` (`module_id`, `person_id`) VALUES ('giftcards',1);

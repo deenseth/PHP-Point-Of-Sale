a1b4f2eacaa1a84b10e732dcb4b751649f75d988
Robert Elwell <relwell@aetherquest.com>
2011-03-16 22:00:54 -0700

    scaffolding link added to dashboard

--- application/views/mailchimpdash/index.php
+++ application/views/mailchimpdash/index.php
@@ -2,4 +2,11 @@
 <br />
 <h3><?php echo $this->lang->line('common_mailchimp_dashboard'); ?></h3>
 <h4><?php echo $this->lang->line('common_mailchimp_dashboard_desc'); ?></h4>
+<div id="dashboard-links">
+    <ul>
+        <li><?=anchor(current_url().'/lists', 'Lists')?></li>
+    
+    
+    </ul>
+</div>
 <?php $this->load->view("partial/footer"); ?>

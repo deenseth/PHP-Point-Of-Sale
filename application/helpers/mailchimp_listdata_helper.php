277c69627937de0cda8e35c1ef2940cd54be5ebf
Robert Elwell <relwell@aetherquest.com>
2011-03-18 14:34:56 -0700

    Update width on button links to get right-sized modal window

--- application/helpers/mailchimp_listdata_helper.php
+++ application/helpers/mailchimp_listdata_helper.php
@@ -17,9 +17,9 @@ function display_email_data($person, $list, $filters)
         $table_data_row.='<td width="12%">'.character_limiter($person->first_name,13).'</td>';
         $table_data_row.='<td width="24%" class="email">'.mailto($person->email,character_limiter($person->email,22)).'</td>';
         $table_data_row.='<td width="50%" class="action">'
-                       .anchor("customers/view/-1/width:500,email:".$person->email, $CI->lang->line('customers_new'),array('class'=>'thickbox left pill button','title'=>$CI->lang->line('customers_update'),'onClick'=>'thickit(this); return false;'))
-                       .anchor("suppliers/view/-1/width:500email:".$person->email, $CI->lang->line('suppliers_new'),array('class'=>'thickbox middle pill button','title'=>$CI->lang->line('suppliers_update'),'onClick'=>'thickit(this); return false;'))
-                       .anchor("employees/view/-1/width:500,email:".$person->email, $CI->lang->line('employees_new'),array('class'=>'thickbox middle pill button','title'=>$CI->lang->line('employees_update'),'onClick'=>'thickit(this); return false;'))
+                       .anchor("customers/view/-1/width:350,email:".$person->email, $CI->lang->line('customers_new'),array('class'=>'thickbox left pill button','title'=>$CI->lang->line('customers_update'),'onClick'=>'thickit(this); return false;'))
+                       .anchor("suppliers/view/-1/width:360,email:".$person->email, $CI->lang->line('suppliers_new'),array('class'=>'thickbox middle pill button','title'=>$CI->lang->line('suppliers_update'),'onClick'=>'thickit(this); return false;'))
+                       .anchor("employees/view/-1/width:650,email:".$person->email, $CI->lang->line('employees_new'),array('class'=>'thickbox middle pill button','title'=>$CI->lang->line('employees_update'),'onClick'=>'thickit(this); return false;'))
                        .'<a class="negative button remove pill right"' 
                        .' onClick="listremove(this)">'
                        .'Remove</a>'
@@ -37,12 +37,24 @@ function display_email_data_for_person($person, $persontype)
     
     $controller_name = strtolower($persontype).'s';
     
+    switch($persontype) {
+        case 'Customer':
+            $width = 350;
+            break;
+        case 'Supplier':
+            $width = 360;
+            break;
+        case 'Employee':
+            $width = 650;
+            break;
+    }
+    
     $table_data_row='<tr>';
     $table_data_row.='<td width="13%">'.character_limiter($person->last_name,13).'</td>';
     $table_data_row.='<td width="13%">'.character_limiter($person->first_name,13).'</td>';
     $table_data_row.='<td width="24%" class="email">'.mailto($person->email,character_limiter($person->email,22)).'</td>';
     $table_data_row.='<td width="50%" class="action">'
-                   .anchor($controller_name."/view/$person->person_id/width:500", $CI->lang->line($controller_name.'_update'),
+                   .anchor($controller_name."/view/$person->person_id/width:{$width}", $CI->lang->line($controller_name.'_update'),
                         array('class'=>'thickbox button pill left','title'=>$CI->lang->line($controller_name.'_update'), 
                         'onClick'=>'thickit(this); return false;'))
                    .'<a class="negative button remove pill right"' 

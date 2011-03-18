e4833580320b8fc09429395a3b33dcdaa8bec960
Robert Elwell <relwell@aetherquest.com>
2011-03-18 14:23:28 -0700

    Add prettier link to list management under mailchimp dash

--- application/views/mailchimpdash/index.php
+++ application/views/mailchimpdash/index.php
@@ -1,12 +1,13 @@
 <?php $this->load->view("partial/header"); ?>
+<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/mailchimpdash/index.css" />
 <br />
 <h3><?php echo $this->lang->line('common_mailchimp_dashboard'); ?></h3>
 <h4><?php echo $this->lang->line('common_mailchimp_dashboard_desc'); ?></h4>
 <div id="dashboard-links">
-    <ul>
-        <li><?=anchor(current_url().'/lists', 'Lists')?></li>
-    
-    
-    </ul>
+    <div class="dashboard-link">
+        <?=anchor(current_url().'/lists', '<img src="'.base_url().'/images/chimpsmile.jpeg" alt=""/>')?>
+        <p><?=anchor(current_url().'/lists', 'List Management')?></p>
+        <div class="clear"><!--  --></div>
+    </div>
 </div>
 <?php $this->load->view("partial/footer"); ?>

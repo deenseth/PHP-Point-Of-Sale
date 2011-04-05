6425e8d9786856dd4ef7aed31eca8751572f1d19
Chris Muench <me@chrismuench.com>
2011-04-05 16:43:46 -0400

    Using live url for authorize.net

--- application/views/sales/register.php
+++ application/views/sales/register.php
@@ -238,7 +238,7 @@ else
 			if (isset($payments[$this->lang->line('sales_integrated_credit_card')]))
 			{	
 			?>
-			<form method="post" action="<?php echo AuthorizeNetDPM::SANDBOX_URL;?>" id="credit_card_form">
+			<form method="post" action="<?php echo AuthorizeNetDPM::LIVE_URL;?>" id="credit_card_form">
 			       
 			      <?php echo $sim->getHiddenFieldString();
 			    ?>

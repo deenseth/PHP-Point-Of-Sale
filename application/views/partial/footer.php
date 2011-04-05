878ed96ba6d25c2c00a4071dc28b2063bc8178f0
Chris Muench <me@chrismuench.com>
2011-04-04 20:21:35 -0400

    added whats new and bumped version to 11.3

--- application/views/partial/footer.php
+++ application/views/partial/footer.php
@@ -1,6 +1,6 @@
 </div>
 </div>
-<div id="footer"> <?php echo $this->lang->line('common_you_are_using_phppos')?> 11.2.
+<div id="footer"> <?php echo $this->lang->line('common_you_are_using_phppos')?> 11.3.
 <?php echo $this->lang->line('common_please_visit_my'); ?> <a href="http://www.phppointofsale.com" target="_blank"><?php echo $this->lang->line('common_website'); ?></a> <?php echo $this->lang->line('common_learn_about_project'); ?>.</div>
 </body>
 </html>

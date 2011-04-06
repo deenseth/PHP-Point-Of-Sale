6543f83b6342ee11432534f1b698632cfbe5c742
Robert Elwell <relwell@aetherquest.com>
2011-04-05 19:31:18 -0700

    continued progress turning report generation into a service

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -123,89 +123,29 @@ class Reports extends Secure_area
 	//Graphical summary customers report
 	function graphical_summary_categories($start_date, $end_date)
 	{
-		$this->load->model('reports/Summary_categories');
-		$model = $this->Summary_categories;
-
-		$data = array(
-			"title" => $this->lang->line('reports_categories_summary_report'),
-			"data_file" => site_urL("reports/graphical_summary_categories_graph/$start_date/$end_date"),
-			"subtitle" => date('m/d/Y', strtotime($start_date)) .'-'.date('m/d/Y', strtotime($end_date)),
-			"summary_data" => $model->getSummaryData(array('start_date'=>$start_date, 'end_date'=>$end_date))
-		);
-
-		$this->load->view("reports/graphical",$data);
+		$this->Service->graphical_summary_categories($start_date, $end_date);
 	}
 	
 	//The actual graph data
 	function graphical_summary_categories_graph($start_date, $end_date)
 	{
-		$this->load->model('reports/Summary_categories');
-		$model = $this->Summary_categories;
-		$report_data = $model->getData(array('start_date'=>$start_date, 'end_date'=>$end_date));
-		
-		$graph_data = array();
-		foreach($report_data as $row)
-		{
-			$graph_data[$row['category']] = $row['total'];
-		}
-		
-		$data = array(
-			"title" => $this->lang->line('reports_categories_summary_report'),
-			"data" => $graph_data
-		);
-
-		$this->load->view("reports/graphs/pie",$data);
+		$this->Service->graphical_summary_categories_graph($start_date, $end_date);
 	}
 	
 	function graphical_summary_suppliers($start_date, $end_date)
 	{
-		$this->load->model('reports/Summary_suppliers');
-		$model = $this->Summary_suppliers;
-
-		$data = array(
-			"title" => $this->lang->line('reports_suppliers_summary_report'),
-			"data_file" => site_urL("reports/graphical_summary_suppliers_graph/$start_date/$end_date"),
-			"subtitle" => date('m/d/Y', strtotime($start_date)) .'-'.date('m/d/Y', strtotime($end_date)),
-			"summary_data" => $model->getSummaryData(array('start_date'=>$start_date, 'end_date'=>$end_date))
-		);
-
-		$this->load->view("reports/graphical",$data);
+		$this->Service->graphical_summary_suppliers($start_date, $end_date);
 	}
 	
 	//The actual graph data
 	function graphical_summary_suppliers_graph($start_date, $end_date)
 	{
-		$this->load->model('reports/Summary_suppliers');
-		$model = $this->Summary_suppliers;
-		$report_data = $model->getData(array('start_date'=>$start_date, 'end_date'=>$end_date));
-		
-		$graph_data = array();
-		foreach($report_data as $row)
-		{
-			$graph_data[$row['supplier']] = $row['total'];
-		}
-		
-		$data = array(
-			"title" => $this->lang->line('reports_suppliers_summary_report'),
-			"data" => $graph_data
-		);
-
-		$this->load->view("reports/graphs/pie",$data);
+		$this->Service->graphical_summary_suppliers_graph($start_date, $end_date);
 	}
 	
 	function graphical_summary_employees($start_date, $end_date)
 	{
-		$this->load->model('reports/Summary_employees');
-		$model = $this->Summary_employees;
-
-		$data = array(
-			"title" => $this->lang->line('reports_employees_summary_report'),
-			"data_file" => site_urL("reports/graphical_summary_employees_graph/$start_date/$end_date"),
-			"subtitle" => date('m/d/Y', strtotime($start_date)) .'-'.date('m/d/Y', strtotime($end_date)),
-			"summary_data" => $model->getSummaryData(array('start_date'=>$start_date, 'end_date'=>$end_date))
-		);
-
-		$this->load->view("reports/graphical",$data);
+		$this->Service->graphical_summary_employees($start_date, $end_date);
 	}
 	
 	//The actual graph data
--- application/libraries/report_service.php
+++ application/libraries/report_service.php
@@ -308,6 +308,111 @@ class Report_Service
 		$this->_display($this->CI->load->view("reports/graphs/hbar",$data,true));
 	}
 	
+    function graphical_summary_categories($start_date, $end_date)
+	{
+		$this->CI->load->model('reports/Summary_categories');
+		$model = $this->CI->Summary_categories;
+
+		$data = array(
+			"title" => $this->CI->lang->line('reports_categories_summary_report'),
+			"data_file" => site_urL("reports/graphical_summary_categories_graph/$start_date/$end_date"),
+			"subtitle" => date('m/d/Y', strtotime($start_date)) .'-'.date('m/d/Y', strtotime($end_date)),
+			"summary_data" => $model->getSummaryData(array('start_date'=>$start_date, 'end_date'=>$end_date))
+		);
+
+		$this->_display($this->CI->load->view("reports/graphical",$data,true));
+	}
+	
+    function graphical_summary_categories_graph($start_date, $end_date)
+	{
+		$this->CI->load->model('reports/Summary_categories');
+		$model = $this->CI->Summary_categories;
+		$report_data = $model->getData(array('start_date'=>$start_date, 'end_date'=>$end_date));
+		
+		$graph_data = array();
+		foreach($report_data as $row)
+		{
+			$graph_data[$row['category']] = $row['total'];
+		}
+		
+		$data = array(
+			"title" => $this->CI->lang->line('reports_categories_summary_report'),
+			"data" => $graph_data
+		);
+
+		$this->_display($this->CI->load->view("reports/graphs/pie",$data,true));
+	}
+	
+    function graphical_summary_suppliers($start_date, $end_date)
+	{
+		$this->CI->load->model('reports/Summary_suppliers');
+		$model = $this->CI->Summary_suppliers;
+
+		$data = array(
+			"title" => $this->CI->lang->line('reports_suppliers_summary_report'),
+			"data_file" => site_urL("reports/graphical_summary_suppliers_graph/$start_date/$end_date"),
+			"subtitle" => date('m/d/Y', strtotime($start_date)) .'-'.date('m/d/Y', strtotime($end_date)),
+			"summary_data" => $model->getSummaryData(array('start_date'=>$start_date, 'end_date'=>$end_date))
+		);
+
+		$this->_display($this->CI->load->view("reports/graphical",$data,true));
+	}
+	
+    function graphical_summary_suppliers_graph($start_date, $end_date)
+	{
+		$this->CI->load->model('reports/Summary_suppliers');
+		$model = $this->CI->Summary_suppliers;
+		$report_data = $model->getData(array('start_date'=>$start_date, 'end_date'=>$end_date));
+		
+		$graph_data = array();
+		foreach($report_data as $row)
+		{
+			$graph_data[$row['supplier']] = $row['total'];
+		}
+		
+		$data = array(
+			"title" => $this->CI->lang->line('reports_suppliers_summary_report'),
+			"data" => $graph_data
+		);
+
+		$this->_display($this->CI->load->view("reports/graphs/pie",$data,true));
+	}
+	
+    function graphical_summary_employees($start_date, $end_date)
+	{
+		$this->CI->load->model('reports/Summary_employees');
+		$model = $this->CI->Summary_employees;
+
+		$data = array(
+			"title" => $this->CI->lang->line('reports_employees_summary_report'),
+			"data_file" => site_urL("reports/graphical_summary_employees_graph/$start_date/$end_date"),
+			"subtitle" => date('m/d/Y', strtotime($start_date)) .'-'.date('m/d/Y', strtotime($end_date)),
+			"summary_data" => $model->getSummaryData(array('start_date'=>$start_date, 'end_date'=>$end_date))
+		);
+
+		$this->_display($this->CI->load->view("reports/graphical",$data,true));
+	}
+	
+    function graphical_summary_employees_graph($start_date, $end_date)
+	{
+		$this->CI->load->model('reports/Summary_employees');
+		$model = $this->CI->Summary_employees;
+		$report_data = $model->getData(array('start_date'=>$start_date, 'end_date'=>$end_date));
+		
+		$graph_data = array();
+		foreach($report_data as $row)
+		{
+			$graph_data[$row['employee']] = $row['total'];
+		}
+		
+		$data = array(
+			"title" => $this->CI->lang->line('reports_employees_summary_report'),
+			"data" => $graph_data
+		);
+
+		$this->_display($this->CI->load->view("reports/graphs/pie",$data,true));
+	}
+	
 	
     /**
      * Sets the option to suppress echo and return HTML.

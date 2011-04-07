4b2244544c1d7bb8bcbc28d948c4391ec5d3f203
Chris Muench <me@chrismuench.com>
2011-04-06 21:50:52 -0400

    Workaround for session regeneration bug that causes sessions to expire

--- application/config/config.php
+++ application/config/config.php
@@ -239,7 +239,7 @@ $config['sess_use_database']	= TRUE;
 $config['sess_table_name']		= 'sessions';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
-$config['sess_time_to_update'] 		= 300;
+$config['sess_time_to_update'] 	= PHP_INT_MAX;
 
 /*
 |--------------------------------------------------------------------------

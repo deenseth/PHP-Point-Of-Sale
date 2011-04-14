2fe221fea421d771ea28f0883f70f9869e2c624d
Robert Elwell <relwell@aetherquest.com>
2011-04-13 20:36:16 -0700

    added a link to home page in upper left hand corner like every other website has had for the last 7 years

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -32,7 +32,7 @@ html {
 <div id="menubar">
 	<div id="menubar_container">
 		<div id="menubar_company_info">
-		<span id="company_title"><?php echo $this->config->item('company'); ?></span><br />
+		<span id="company_title"><a href="<?=site_url('home')?>" style="text-decoration: none;"><?php echo $this->config->item('company'); ?></a></span><br />
 		<span style='font-size:8pt;'><?php echo $this->lang->line('common_powered_by').' PHP Point Of Sale'; ?></span>
 	</div>
 

89cbf165f436be1e1f0650fa15ff6dacc0a9ce80
Chris Muench <me@chrismuench.com>
2011-04-21 12:56:54 -0400

    Revert "Removed report service and reverted reports back to how they were before mailchimp"
    This reverts commit c014d21f77542f2ecb3910c8182e586afa5486f0.

--- application/views/reports/graphical.php
+++ application/views/reports/graphical.php
@@ -4,15 +4,12 @@ $this->load->view("partial/header");
 <div id="page_title" style="margin-bottom:8px;"><?php echo $title ?></div>
 <div id="page_subtitle" style="margin-bottom:8px;"><?php echo $subtitle ?></div>
 <div style="text-align: center;">
-
 <script type="text/javascript">
 swfobject.embedSWF(
 "<?php echo base_url(); ?>open-flash-chart.swf", "chart",
 "100%", "100%", "9.0.0", "expressInstall.swf",
 {"data-file":"<?php echo $data_file; ?>"} )
 </script>
-<?php
-?>
 </div>
 <div id="chart_wrapper">
 	<div id="chart"></div>
@@ -22,6 +19,7 @@ swfobject.embedSWF(
 	<div class="summary_row"><?php echo $this->lang->line('reports_'.$name). ': '.to_currency($value); ?></div>
 <?php }?>
 </div>
+<div id="feedback_bar"></div>
 <?php
 $this->load->view("partial/footer"); 
 ?>

91fcd4850a5354941270d7cfc5402a4577ee9f2a
Shaun Kutch <shaun.kutch@doublebeam.com>
2014-07-13 17:13:41 -0700

    Added error catching

--- application/models/sync_items.php
+++ application/models/sync_items.php
@@ -5,9 +5,136 @@ class Sync_items extends CI_Model
 	private $session;
 
 	function __construct() {
-    	$client = new SoapClient('https://local.fairytale-boutique.com/api/v2_soap?wsdl=1');
-    	//get a session token
-    	$session = $client->login('raspberrypos', '99kXNM3zrNPr');
+		try {
+	    	$this->client = new SoapClient('https://local.fairytale-boutique.com/api/v2_soap?wsdl=1');
+	    	//get a session token
+	    	$this->session = $this->client->login('raspberrypos', '99kXNM3zrNPr');
+	    } catch (SoapFault $fault) {
+			$this->log("SOAP Fault: (faultcode: {$fault->faultcode}, faultstring: {$fault->faultstring})");
+		}
+   	}
+
+   	function sync_sales()
+   	{
+   		//Get the last sync time
+		$last_sync = $this->get_last_sync();
+		//Get items sold since last sync
+		$items_sold = $this->Sale->get_sale_items_after($last_sync);
+		
+		try {
+			//Update inventory on remote server
+			foreach ($items_sold as $sold_item) {
+				$stock_item = $this->client->catalogInventoryStockItemList($this->session, array($sold_item->item_number));
+				$qty = $stock_item[0]->qty;
+				$new_qty = $qty - $sold_item->quantity_purchased;
+				$result = $this->client->catalogInventoryStockItemUpdate($this->session, $sold_item->item_number, array(
+					'qty' => $new_qty
+				));
+				$this->log("Updated remote inventory for: " . $sold_item->item_number);
+			}
+
+			//Save the timestamp so we know the last sync time
+			$this->save_sync_time();
+
+		} catch (SoapFault $fault) {
+			$this->log("SOAP Fault: (faultcode: {$fault->faultcode}, faultstring: {$fault->faultstring})");
+		}
+   	}
+
+   	function sync_inventory()
+   	{
+   		try {
+	   		//Get all products on remote server
+			$list = $this->client->catalogProductList($this->session);
+			for($i = 0; $i < count($list); $i++){
+				$current = $list[$i];
+				$attributes = new stdClass();
+				$attributes->additional_attributes = array('cost', 'manufacturer', 'price');
+			  	$product = $this->client->catalogProductInfo($this->session, $current->product_id, null, $attributes);
+			  	$productInventory = $this->client->catalogInventoryStockItemList($this->session, array($current->product_id));
+			  	$price = null;
+			  	$cost = null;
+			  	foreach($product->additional_attributes as $attribute) {
+			  	    if ($attribute->key == "price") {
+			  	        $price = $attribute->value;
+			  	    }
+			  	     if ($attribute->key == "cost") {
+			  	        $cost = $attribute->value;
+			  	    }
+			  	}
+			  	$item_data = array();
+
+			  	if(isset($product->name)){
+			  		$item_data['name'] = $product->name;
+			  	}else{
+			  		$this->log('Name cannot be empty');
+			  		continue;
+			  	}
+			  	if(isset($product->sku)){
+			  		$item_data['item_number'] = $product->sku;
+			  	}else{
+			  		$this->log('sku cannot be empty');
+			  		continue;
+			  	}
+			  	if(isset($cost)){
+			  		$item_data['cost_price'] = $cost;
+			  	}else{
+			  		$item_data['cost_price'] = 0;
+			  	}
+			  	if(isset($price)){
+			  		$item_data['unit_price'] = $price;
+			  	}else{
+			  		$this->log('price cannot be empty');
+			  		continue;
+			  	}
+			  	if(isset($productInventory[0]->qty)){
+			  		$item_data['quantity'] = $productInventory[0]->qty;
+			  	}else{
+			  		$this->log('Quantity cannot be empty');
+			  		continue;
+			  	}
+			  	$item_data['description'] = isset($product->description)? $product->description : '';
+			  	$item_data['category'] = 'Remote Inventory';
+			  	$item_data['supplier_id'] = null;
+			  	$item_data['reorder_level'] = 0;
+			  	$item_data['location'] = 'in store';
+			  	$item_data['allow_alt_description'] = 0;
+			  	$item_data['is_serialized'] = 0;
+		  		$item_data['item_id'] = $this->Item->get_item_id($product->sku);
+		  		
+		  		//If we have a item_id the item already exists so update otherwise we need to add it
+		  		if($item_data['item_id'])
+		  		{
+		  			$update_data[] = $item_data;
+		  		}else{
+		  			$add_data[] = $item_data;
+		  		}
+
+		  		$this->log("Processing: " . $item_data['item_id'] . " " . $item_data['name'] . " " . $item_data['item_number']);
+			}
+
+			//If we have items that need to be updated
+			if(isset($update_data))
+			{
+				$this->Item->update_multiple_by_index($update_data, 'item_id');
+			}
+			//If we have items that need to be added.
+			if(isset($add_data))
+			{
+				$this->Item->save_multiple($add_data);
+
+				foreach ($add_data as $item) {
+					$tax_ids[] = $this->Item->get_item_id($item['item_number']);
+				}
+
+				$items_taxes_data[] = array('name'=>'DEFAULT TAX', 'percent'=>10);
+				$this->Item_taxes->save_multiple($items_taxes_data, $tax_ids);
+			}
+
+			$this->log("Inventory now synced");
+		} catch (SoapFault $fault) {
+			$this->log("SOAP Fault: (faultcode: {$fault->faultcode}, faultstring: {$fault->faultstring})");
+		}
    	}
 
 	/*
@@ -31,6 +158,11 @@ class Sync_items extends CI_Model
 		{
 			return $query->row()->sync_time;
 		}
+	}
+
+	function log($message)
+	{
+		echo $message . "\r\n";
 	}
 }
 ?>

53cca17b19ad14565157cb62e1ec255cc0592c73
FastAct <93490087+FastAct@users.noreply.github.com>
2023-05-07 09:58:09 +0200

    Add Dutch translation

---
+++ application/language/nederlands/customers_lang.php
@@ -0,0 +1,16 @@
+<?php
+$lang['customers_new']='Nieuwe Klant';
+$lang['customers_customer']='Klant';
+$lang['customers_update']='Klant Bijwerken';
+$lang['customers_confirm_delete']='Weet je zeker dat je de geselecteerde klanten wilt verwijderen?';
+$lang['customers_none_selected']='Je hebt geen klanten geselecteerd om te verwijderen';
+$lang['customers_error_adding_updating'] = 'Fout bij toevoegen/bijwerken klant';
+$lang['customers_successful_adding']='Je hebt met succes een klant toegevoegd';
+$lang['customers_successful_updating']='Je hebt met succes de klant bijgewerkt';
+$lang['customers_successful_deleted']='Je hebt met succes verwijderd';
+$lang['customers_one_or_multiple']='klant(en)';
+$lang['customers_cannot_be_deleted']='Kon geselecteerde klanten niet verwijderen, een of meer van de geselecteerde klanten heeft nog verkopen.';
+$lang['customers_basic_information']='Klanteninformatie';
+$lang['customers_account_number']='Account #';
+$lang['customers_taxable']='Belastbaar';
+?>
